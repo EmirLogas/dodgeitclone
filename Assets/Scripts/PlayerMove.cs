fd7fbe7d77dd0275f8b0a331192762b7e668b9cc
Emir <76751818+EmirLogas@users.noreply.github.com>
2021-09-24 01:56:35 +0300

    Movement Fix

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float forcePowerUp = 4;
-    public float forcePowerHorizontal = 0.15f;
+    public float forcePowerUp = 150;
+    public float forcePowerHorizontal = 1;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcePowerUp) * Time.deltaTime);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, forcePowerUp));
         }
         if (Input.GetKey(KeyCode.A))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(-forcePowerHorizontal, 0) * Time.deltaTime);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-forcePowerHorizontal, 0.0f));
         }
         if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(forcePowerHorizontal, 0) * Time.deltaTime);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(forcePowerHorizontal, 0.0f));
         }
     }
 }
