9c722511f68144e10131df04537aae03b5622678
Emir <76751818+EmirLogas@users.noreply.github.com>
2021-09-24 01:23:52 +0300

    Movable Player

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float forcePowerUp = 4;
+    public float forcePowerHorizontal = 1;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcePowerUp) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-forcePowerHorizontal, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(forcePowerHorizontal, 0) * Time.deltaTime);
+        }
+    }
+}
