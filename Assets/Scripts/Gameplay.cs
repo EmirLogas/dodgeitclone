474486dc7ccfd9e954fcde35ef55eef4943b9437
Emir <76751818+EmirLogas@users.noreply.github.com>
2021-09-25 14:13:26 +0300

    Frozen and Dead
    added Frozen and unfrozen voids, dead boolean.

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -12,18 +12,19 @@ public class Gameplay : MonoBehaviour
     Rigidbody2D rb;
     public bool IsFrozen2;
     public GameObject pressWtext;
-    // Start is called before the first frame update
+    public bool isDead;
+
     void Start()
     {
-        rb= GetComponent<Rigidbody2D>();
+        IsFrozen2 = true;
+        rb = GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         GameObject a = Instantiate(blackPrefab) as GameObject;
-        a.transform.position = new Vector2(Random.Range(-screenBounds.x,screenBounds.x), Random.Range(-screenBounds.y, screenBounds.y));
+        a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), Random.Range(-screenBounds.y, screenBounds.y));
         GameObject b = Instantiate(redPrefab) as GameObject;
         b.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), Random.Range(-screenBounds.y, screenBounds.y));
-        bool IsFrozen = (rb.constraints & RigidbodyConstraints2D.FreezePosition) == RigidbodyConstraints2D.FreezePosition;
-        IsFrozen2 = IsFrozen;
+
     }
     void SpawnObject(int redCount)
     {
@@ -35,13 +36,13 @@ public class Gameplay : MonoBehaviour
             b.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), Random.Range(-screenBounds.y, screenBounds.y));
         }
     }
-    // Update is called once per frame
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name.Contains("redDot"))
         {
-            Debug.Log("kek");
-            Application.Quit();
+            isDead = true;
+            FrozePlayer();
         }
         else if (other.gameObject.name.Contains("BlackDot"))
         {
@@ -51,18 +52,27 @@ public class Gameplay : MonoBehaviour
             txt.text = score.ToString();
             SpawnObject(redCount);
         }
-        
     }
 
     public void Update()
     {
-        
-
-        if (IsFrozen2==true && Input.GetKeyDown("w"))
+        if (IsFrozen2 == true && Input.GetKeyDown("w") && isDead == false)
         {
-            IsFrozen2 = false;
-            rb.constraints = RigidbodyConstraints2D.None;
-            pressWtext.gameObject.SetActive(false);
+            UnFrozePlayer();
         }
+    }
+
+    public void FrozePlayer()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+        bool IsFrozen = (rb.constraints & RigidbodyConstraints2D.FreezePosition) == RigidbodyConstraints2D.FreezePosition;
+        IsFrozen2 = IsFrozen;
+    }
+
+    public void UnFrozePlayer()
+    {
+        IsFrozen2 = false;
+        rb.constraints = RigidbodyConstraints2D.None;
+        pressWtext.gameObject.SetActive(false);
     }
 }
