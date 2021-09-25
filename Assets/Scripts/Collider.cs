391bdfb4d7c6421da119459cfde1bba749f0becb
Emir <76751818+EmirLogas@users.noreply.github.com>
2021-09-25 15:58:26 +0300

    Collider finish i think

--- Assets/Scripts/Collider.cs
+++ Assets/Scripts/Collider.cs
@@ -46,19 +46,13 @@ public class Collider : MonoBehaviour
         screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
 
         //Change our scale and positions to match the edges of the screen...   
-        rightCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        rightCollider.position = new Vector3(cameraPos.x + screenSize.x + (rightCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
-        leftCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        leftCollider.position = new Vector3(cameraPos.x - screenSize.x - (leftCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
-        topCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        topCollider.position = new Vector3(cameraPos.x, cameraPos.y + screenSize.y + (topCollider.localScale.y * 0.5f), zPosition);
-        bottomCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        bottomCollider.position = new Vector3(cameraPos.x, cameraPos.y - screenSize.y - (bottomCollider.localScale.y * 0.5f), zPosition);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        rightCollider.localScale = new Vector3(colDepth, screenSize.y * 2 + 2, colDepth);
+        rightCollider.position = new Vector3(cameraPos.x + screenSize.x + (rightCollider.localScale.x * 0.5f) + 0.80f, cameraPos.y, zPosition);
+        leftCollider.localScale = new Vector3(colDepth, screenSize.y * 2 + 2, colDepth);
+        leftCollider.position = new Vector3(cameraPos.x - screenSize.x - (leftCollider.localScale.x * 0.5f) - 0.80f, cameraPos.y, zPosition);
+        topCollider.localScale = new Vector3(screenSize.x * 2 + 2, colDepth, colDepth);
+        topCollider.position = new Vector3(cameraPos.x, cameraPos.y + screenSize.y + (topCollider.localScale.y * 0.5f) + 0.80f, zPosition);
+        bottomCollider.localScale = new Vector3(screenSize.x * 2 + 2, colDepth, colDepth);
+        bottomCollider.position = new Vector3(cameraPos.x, cameraPos.y - screenSize.y - (bottomCollider.localScale.y * 0.5f) - 0.80f, zPosition);
     }
 }
